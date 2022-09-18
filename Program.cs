50ab38d4746fd49b2e33b4d46a7c9ef6d17c5f01
Alexey <forsage@list.ru>
2022-09-18 21:24:25 +0300

    Добавил метод создания строкового массива CreateArray

--- Program.cs
+++ Program.cs
@@ -12,3 +12,24 @@ void ShowArray(string[] array)
     }
     System.Console.WriteLine();
 }
+string[] CreateArray()
+{
+    int count = 0;
+    string value = "";
+    string[] tempArray = new string[1000];
+    System.Console.WriteLine($"Для завершения ввода введите 'exit' ");
+    for (int count = 0; count < array.Length; count++)
+    {
+        value = Prompt($"Введите значение {count+1} ");
+        if (value != "exit")
+        {
+            tempArray [count] = value;
+        }
+    }
+    string [] array = new string[count-1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = tempArray[i];
+    }
+    return array;
+}
